3c8b1e48221e06ba4ea75349102cf3789a6d09ce
cefet <cefet@localhost.localdomain>
2018-12-19 11:14:58 -0200

    Desing modificado

--- index.php
+++ index.php
@@ -4,31 +4,27 @@
     $listar = $editor->listarArquivos();
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Envio de arquivo</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </head>
-  <style>
-    body{
-      background-image: url("imagens/fundo_6.jpg");
-    }
-    .jumbotron{
-      background-image: url("imagens/fundo_j_11.png");
 
-    }
-  </style>
-  <body>
+<!DOCTYPE html>
+<html lang = "pt-br">
+    <head>
+        <meta charset = "utf-8">
+        <title>Upload</title>
+        <link rel = "icon" href = "imagens/upload.ico">
+        <script src = "js/jquery.js"></script>
+        <link rel = "stylesheet" href = "css/bootstrap.css">
+        <link rel = "stylesheet" href = "css/design.css">
+        <link rel = "stylesheet" href = "css/bootstrap.map.css">
+        <script src = "js/bootstrap.js"></script>
+        </script>
+    </head>
+    <body>
     <div class="jumbotron">
         <br>
         <br>
         <br>
         <div class="container">  
-        <h1>Upload de arquivos&nbsp;&nbsp;&nbsp;<img src="imagens/icone_up.png" class="img-rounded" alt="Cinque Terre" width="80" height="80"> </h1> 
+        <h1>Upload de arquivos&nbsp;&nbsp;&nbsp;<img src="imagens/upload.png" class="img-rounded" alt="Cinque Terre" width="80" height="80"> </h1> 
         </div>
     </div>
     <div class="container">
--- tratarArquivos.php
+++ tratarArquivos.php
@@ -6,31 +6,26 @@
 
 ?>
 
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Tratamento de arquivos</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </head>
-  <style>
-    body{
-      background-image: url("imagens/fundo_6.jpg");
-    }
-    .jumbotron{
-      background-image: url("imagens/fundo_j_11.png");
-
-    }
-  </style>
-  <body>
+  <!DOCTYPE html>
+  <html lang = "pt-br">
+    <head>
+        <meta charset = "utf-8">
+        <title>Tratamento de Arquivos</title>
+        <link rel = "icon" href = "imagens/opcoes.ico">
+        <script src = "js/jquery.js"></script>
+        <link rel = "stylesheet" href = "css/bootstrap.css">
+        <link rel = "stylesheet" href = "css/design.css">
+        <link rel = "stylesheet" href = "css/bootstrap.map.css">
+        <script src = "js/bootstrap.js"></script>
+        </script>
+    </head>
+    <body>
     <div class="jumbotron">
         <br>
         <br>
         <br>
         <div class="container">  
-        <h1>Tratamento de Arquivos&nbsp;&nbsp;&nbsp;<img src="imagens/icone_up.png" class="img-rounded" alt="Cinque Terre" width="80" height="80"> </h1> 
+        <h1>Tratamento de Arquivos&nbsp;&nbsp;&nbsp;<img src="imagens/opcoes.png" class="img-rounded" alt="Cinque Terre" width="80" height="80"> </h1> 
         </div>
     </div>
     <div class="container">
@@ -41,7 +36,8 @@
           <?php foreach($parametros as $var): ?>
           <input type="checkbox" name="parametros" value="<?php echo $var; ?>">&nbsp;&nbsp;<?php echo $var; ?></input></br>
           <?php endforeach; ?>
-          <a href="tratarArquivos.php"><button type="button" class="btn btn-info">OK</button></a>
+          </br>
+          <input type="submit" value="OK" class="btn btn-info"/>
           </form>
           <!--
           <h2>Digite a palavra pela qual deseja modificar</h2></br>
